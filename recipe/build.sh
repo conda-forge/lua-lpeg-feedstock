0e73088f8e5848ce63834c453f96d583f73a647e
Pavel Zwerschke <pavelzw@gmail.com>
2024-11-29 12:24:18 +0100

    pkgconfig

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,6 @@ export LUA_INCLUDE_DIR=$PREFIX/include/luajit-2.1
 cmake -S . -B build \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DLUA_INCLUDE_DIR=$LUA_INCLUDE_DIR \
     $CMAKE_ARGS
 cmake --build build --config Release
 cmake --install build
